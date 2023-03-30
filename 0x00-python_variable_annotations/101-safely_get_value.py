db589280586597059428cb1941c053f3de56886f
WairimuMaringa <makena96wairimu@gmail.com>
2023-03-30 13:33:51 -0700

    advanced two

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Return values and annotations of given parameters
+"""
+from typing import Sequence, Union, Any, Mapping, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """ Returns a value. """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
