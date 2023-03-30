30d6bc857e104acfbff2481508c1a4606bc679a5
WairimuMaringa <makena96wairimu@gmail.com>
2023-03-30 05:04:16 -0700

    task zero

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Type annotated funtion add that takes float a and b
+as arguments and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Reutrn sum. """
+    return a + b
