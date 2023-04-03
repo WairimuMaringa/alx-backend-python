222c2fcbbc9d37d129cfea4344604e94b124568f
WairimuMaringa <makena96wairimu@gmail.com>
2023-04-03 07:54:06 -0700

    task one

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Import from task zero and write an async routine that
+takes two int args. Return list of delays in ascending order
+"""
+import aysncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Return list of delays. """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
